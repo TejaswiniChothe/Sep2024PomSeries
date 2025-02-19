f8d042c858b6a1f872c9f0101d266d8fe4c1debd
TejaswiniChothe <tejaswini.chothe11@gmail.com>
2025-02-19 21:03:53 +0530

    modified login page code

--- src/main/java/com/qa/opecart/pages/dummyLoginpage.java
+++ src/main/java/com/qa/opecart/pages/dummyLoginpage.java
@@ -1,10 +1,13 @@
 package com.qa.opecart.pages;
 
+import org.openqa.selenium.By;
+
 public class dummyLoginpage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		By loginbutton=By.id("login");
 		System.out.println("Hello world");
 	}
 
