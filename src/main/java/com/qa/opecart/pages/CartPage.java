bd0cf862d2e3221192fb1087d774dd3408094b73
TejaswiniChothe <tejaswini.chothe11@gmail.com>
2025-02-19 21:12:10 +0530

    cart page chnages added

---
+++ src/main/java/com/qa/opecart/pages/CartPage.java
@@ -0,0 +1,13 @@
+package com.qa.opecart.pages;
+
+import org.openqa.selenium.By;
+
+public class CartPage {
+
+	private By product=By.id("test");
+	
+	public void testproduct()
+	{
+		System.out.println("test product");
+	}
+}
